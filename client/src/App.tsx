e0dcf4e52ec6331a9bd1cd2ebb2f378269a9859b
José León Alarcón <40124504+Nicromano@users.noreply.github.com>
2022-08-31 18:15:31 -0500

    Integrar ReactJS con Typescript

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,25 +1,15 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const peticion = ()=>{
-    fetch('/api/', {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    })
-    .then(response => response.json())
-    .then(json => console.log(json))
-  }
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <button onClick={peticion}>peticxxxxxsaassion</button>
         <a
           className="App-link"
           href="https://reactjs.org"
