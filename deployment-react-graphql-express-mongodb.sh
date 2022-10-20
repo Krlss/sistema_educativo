8d321aa90605555f9c81747f169b6b4b1f37373f
nicromano <ljose297@gmail.com>
2022-10-20 16:12:14 -0500

    Preparando contenedores

---
+++ deployment-react-graphql-express-mongodb.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+# Let other container still running as long as they have different name
+echo "Stop Docker Container ..."
+# List of container names to be stopped
+Container="web-app api mongodb webserver"
+for container_name in $Container; do
+    if [ "$(docker ps -q -f name=$container_name)" ]; then
+        if [ $(docker inspect -f '{{.State.Running}}' $container_name) = "true" ]; then
+            echo "Stop Container: "$container_name
+            docker stop $container_name
+        fi
+    fi
+done
+
+if [[ $(docker ps -a | grep -i Exited) ]]; then
+    echo "Remove Container...";
+    docker rm $(docker ps -a | grep -i Exited | grep -Eo '^[^ ]+');
+fi
+if [[ $(docker images -a | grep -i ago) ]]; then
+    echo "Remove Images...";
+    docker image prune -af;
+fi
+if [[ $(docker volume ls -f dangling=true | grep local) ]]; then
+    echo "Clean up orphaned volumes"
+    docker volume rm $(docker volume ls -qf dangling=true);
+fi
+if [[ $(docker network ls| grep local) ]]; then
+    echo "Clean up orphaned networks";
+    docker network prune $(docker network ls -q);
+fi
+
+echo "Start Docker Compose ..."
+docker-compose up -d
+
+# The database is not ready when the api container starts so the api container has to restart again.
+sleep 10;
+docker restart api;
+
+Container="web-app api mongodb webserver"
+for container_name in $Container; do
+    if [ "$(docker ps -q -f name=$container_name)" ]; then
+        if [ ! $(docker inspect -f '{{.State.Running}}' $container_name) = "true" ]; then
+            echo "Container not running: "$container_name
+            docker ps -a
+            exit
+        fi
+    fi
+done
+
+docker ps -a
+
+echo
+echo "Call URL at http://localhost:3000/"
+echo
+echo "** Deployment successful **"
--- server/src/index.ts
+++ server/src/index.ts
@@ -4,7 +4,7 @@ import {AppDataSource} from './config/typeorm';
 import { start } from "./app";
 async function main() {
   const app =await start();
-  app.listen(3000);
+  app.listen(8000);
   console.log('Server corriendo en puerto 3000');
 }
 
