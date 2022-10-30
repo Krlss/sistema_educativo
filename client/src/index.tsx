230a2616d68c944dd4ffa6a4b4d99c576e155f03
Carlos Pico <cpico6375@gmail.com>
2022-10-30 17:51:15 -0500

    Probando apollo/client

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,78 +2,25 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App'
-
-/* import { ApolloProvider } from 'react-apollo';
-import { ApolloClient } from 'apollo-client';
-import { getMainDefinition } from 'apollo-utilities';
-import { ApolloLink, split } from 'apollo-link';
-import { HttpLink } from 'apollo-link-http';
-import { WebSocketLink } from 'apollo-link-ws';
-import { onError } from 'apollo-link-error';
-import { InMemoryCache } from 'apollo-cache-inmemory';
-import { signOut } from './components/signout/signout'; */
-
-/* import { signOut } from './components/SignOut'; */
-
-/* const httpLink = new HttpLink({
-  uri: 'http://localhost:8000/graphql',
-});
-
-const wsLink = new WebSocketLink({
-  uri: `ws://localhost:8000/graphql`,
-  options: {
-    reconnect: true,
-  },
-});
-
-const terminatingLink = split(
-  ({ query }) => {
-    const {kind, name } = getMainDefinition(query);
-    return (kind === 'OperationDefinition' && name === 'subscription');
-  },
-  wsLink,
-  httpLink,
-);
-
-const authLink = new ApolloLink((operation, forward) => {
-  operation.setContext(({ headers = {} }) => {
-    const token = localStorage.getItem('token');
-
-    if (token) {
-      headers = { ...headers, 'x-token': token };
-    }
-
-    return { headers };
-  });
-
-  return forward(operation);
-});
-
-const errorLink = onError(({ graphQLErrors, networkError }) => {
-  if (graphQLErrors) {
-    graphQLErrors.forEach(({ message, locations, path }) => {
-      console.log('GraphQL error', message);
-
-      if (message === 'UNAUTHENTICATED') {
-        signOut(client);
-      }
-    });
-  }
-
-  if (networkError) {
-    console.log('Network error', networkError);
-      signOut(client);
-  }
-});
-
-const link = ApolloLink.from([authLink, errorLink, terminatingLink]);
-
-const cache = new InMemoryCache();
+import { ApolloClient, HttpLink, gql, InMemoryCache } from '@apollo/client'
 
 const client = new ApolloClient({
-  link,
-  cache,
-}); */
+  cache: new InMemoryCache(),
+  link: new HttpLink({
+    uri: 'http://localhost:8000/graphql'
+  })
+})
+
+const query = gql`
+  query {
+    getUsers {
+      name
+      lastname
+    }
+  }
+`
+
+client.query({ query }).then(result => console.log(result))
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
