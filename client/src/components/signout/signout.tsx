3a2d9f518b90bf108df621fa7adfc77d47153bb8
Carlos Pico <cpico6375@gmail.com>
2022-11-01 22:07:15 -0500

    signout apollo error

--- client/src/components/signout/signout.tsx
+++ client/src/components/signout/signout.tsx
@@ -1,18 +1,7 @@
-import React from 'react'
-import { ApolloConsumer } from 'react-apollo'
-
 /* import * as routes from '../../constants/routes';
 import history from '../../constants/history'; */
 
-const SignOutButton = () => (
-  <ApolloConsumer>
-    {client => (
-      <button type="button" onClick={() => signOut(client)}>
-        Sign Out
-      </button>
-    )}
-  </ApolloConsumer>
-)
+const SignOutButton = () => <></>
 
 const signOut = (client: any) => {
   /* localStorage.removeItem('token');
