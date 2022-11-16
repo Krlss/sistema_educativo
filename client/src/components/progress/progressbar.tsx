f08067e803833adf0796f2f85b197696b756299b
Carlos Pico <cpico6375@gmail.com>
2022-11-15 20:15:41 -0500

    progress bar bg color

--- client/src/components/progress/progressbar.tsx
+++ client/src/components/progress/progressbar.tsx
@@ -4,15 +4,23 @@ interface ProgressBarProps {
 
 const ProgressBar = ({ progress }: ProgressBarProps) => {
   return (
-    <div className="w-full bg-gray-200 rounded-full mt-2">
+    <div className="w-full bg-gray-200 rounded-full mt-2 flex text-xs font-semibold text-center h-3.5 relative">
       <div
-        className={`text-xs font-semibold text-center p-0.5 leading-none rounded-full ${
+        className={`rounded-full ${
           (progress < 20 && 'bg-red-logo') ||
           (progress >= 20 && progress < 50 && 'bg-yellow-page') ||
           (progress >= 50 && progress < 80 && 'bg-green-500') ||
           (progress >= 80 && 'bg-blue-500')
         }`}
-        style={{ width: `${progress}%` }}>
+        style={{ width: `${progress}%` }}
+      />
+      <div
+        className={`absolute right-0 left-0 ${
+          (progress < 20 && 'text-black') ||
+          (progress >= 20 && progress < 50 && 'text-black') ||
+          (progress >= 50 && progress < 80 && 'text-black') ||
+          (progress >= 80 && 'text-white')
+        }`}>
         {progress}%
       </div>
     </div>
