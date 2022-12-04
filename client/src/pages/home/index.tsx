de21726b16349082fb8dfb917c6cc249bb16eef3
Carlos Pico <cpico6375@gmail.com>
2022-12-04 18:07:59 -0500

    dar prueba

--- client/src/pages/home/index.tsx
+++ client/src/pages/home/index.tsx
@@ -2,6 +2,7 @@ import { useContext } from 'react'
 import GeneralContext from '../../contexts/context'
 import HomeCard from '../../components/cards/homeCard'
 import CenterLogo from '../../components/logo/centerLogo'
+import { PROGRESS } from '../../types/contextUser'
 const Home = () => {
   const { user, config } = useContext(GeneralContext)
 
@@ -19,16 +20,48 @@ const Home = () => {
 
       <div className="flex items-center justify-between mb-20">
         <div className="flex flex-wrap mt-4 gap-8">
-          {config.asignatures.map((asignature, index) => (
-            <HomeCard
-              key={index}
-              numberCourse={index + 1}
-              nameCourse={asignature.name}
-              progress={15}
-              to={asignature._id}
-              StringImage={asignature.image}
-            />
-          ))}
+          {config.asignatures.map((asignature, index) => {
+            const findCourse = user.progress.find(
+              course => course.id_asignature === asignature._id
+            ) as PROGRESS
+
+            const sumAllTopicsWithUnit = findCourse?.unit?.reduce((acc, u) => {
+              u.topic?.forEach(t => {
+                acc++
+              })
+              acc++
+              return acc
+            }, 0) as number
+
+            const progress = findCourse.unit?.reduce((acc, u) => {
+              if (u.finished) acc++
+              u.topic?.forEach(t => {
+                if (t.finished) acc++
+              })
+              return acc
+            }, 0) as number
+
+            const canGiveExam = findCourse.unit?.reduce(
+              (acc, u, _, array) => {
+                if (u.finished) acc.unit++
+                if (array.length === acc.unit) acc.can = 1
+                return acc
+              },
+              { can: 0, unit: 0 }
+            ) as { can: number; unit: number }
+
+            return (
+              <HomeCard
+                key={index}
+                numberCourse={index + 1}
+                nameCourse={asignature.name}
+                progress={progress / sumAllTopicsWithUnit ?? 0}
+                to={asignature._id}
+                StringImage={asignature.image}
+                canGiveExam={!!canGiveExam.can}
+              />
+            )
+          })}
         </div>
       </div>
     </>
