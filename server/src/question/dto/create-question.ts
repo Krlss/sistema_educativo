987f55dfbc0cdd4dfdabca0f52abbccf97d81338
Carlos Pico <cpico6375@gmail.com>
2023-02-03 02:02:30 -0500

    validations in DTO create topic and question :safety_vest:

--- server/src/question/dto/create-question.ts
+++ server/src/question/dto/create-question.ts
@@ -1,9 +1,14 @@
 import { InputType, Field } from '@nestjs/graphql';
+import { IsNotEmpty, MinLength } from 'class-validator';
 import { QuestionType } from '../types/question.type';
 
 @InputType()
 export class CreateQuestionDTO {
   @Field({ description: 'Título de la pregunta' })
+  @IsNotEmpty({ message: 'El titulo de la pregunta no puede estar vacío' })
+  @MinLength(2, {
+    message: 'El titulo de la pregunta debe tener al menos 2 caracteres',
+  })
   title: string;
 
   @Field({
@@ -11,14 +16,20 @@ export class CreateQuestionDTO {
     nullable: true,
     defaultValue: '',
   })
+  @IsNotEmpty({ message: 'El subtitulo de la pregunta no puede estar vacío' })
+  @MinLength(2, {
+    message: 'El subtitulo de la pregunta debe tener al menos 2 caracteres',
+  })
   subtitle: string;
 
   @Field(() => QuestionType, {
     description: 'Tipo de pregunta',
   })
+  @IsNotEmpty({ message: 'El tipo de la pregunta no puede estar vacío' })
   type: QuestionType;
 
   @Field({ description: 'Es un objecto, tiene varias formas.' })
+  @IsNotEmpty({ message: 'Las opciones de la pregunta no pueden estar vacías' })
   options: string;
 
   @Field({
--- server/src/topic/dto/create-topic.ts
+++ server/src/topic/dto/create-topic.ts
@@ -1,13 +1,20 @@
 import { InputType, Field } from '@nestjs/graphql';
+import { IsNotEmpty, MinLength } from 'class-validator';
 
 @InputType()
 export class CreateTopicDTO {
   @Field({ description: 'Nombre del tema' })
+  @IsNotEmpty({ message: 'El nombre del tema no puede estar vacío' })
+  @MinLength(2, {
+    message: 'El nombre del tema debe tener al menos 2 caracteres',
+  })
   name: string;
 
   @Field({ description: 'Esta imagen representa la clase del tema' })
+  @IsNotEmpty({ message: 'La imagen del tema no puede estar vacío' })
   image: string;
 
   @Field({ description: 'Este es el vídeo más explicativo del tema' })
+  @IsNotEmpty({ message: 'El vídeo del tema no puede estar vacío' })
   video: string;
 }
