847309930175a3eeec3f7f59e3d79a77800df38b
Carlos Pico <cpico6375@gmail.com>
2023-02-04 20:36:40 -0500

    fix: :bug: Faltaron campos
    Agregué dos campos en el DTO de la asignatura que faltaban, "description" y "image"

--- server/src/asignature/dto/create-asignature.ts
+++ server/src/asignature/dto/create-asignature.ts
@@ -11,6 +11,24 @@ export class CreateAsignatureDTO {
   @MinLength(2, { message: 'El nombre debe tener al menos 2 caracteres' })
   name: string;
 
+  @Field(() => String, {
+    description: 'Descripción de la asignatura',
+  })
+  @IsNotEmpty({
+    message: 'La descripción de la asignatura no puede estar vacío',
+  })
+  @MinLength(25, {
+    message: 'La descripción debe tener al menos 25 caracteres',
+  })
+  description: string;
+
+  @Field(() => String, {
+    description: 'Imagen que represente a la asignatura',
+  })
+  @IsNotEmpty({ message: 'La imagen de la asignatura no puede estar vacío' })
+  @MinLength(2, { message: 'La imagen debe tener al menos 2 caracteres' })
+  image: string;
+
   @Field(() => [Int], {
     description: 'Cursos a los que pertenece la asignatura',
     nullable: true,
