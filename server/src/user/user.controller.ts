9c12ff3550027e5c36fd3dfd1f9097a8df0c0204
Zeroealca <emilioandresalcivarcarrera@gmail.com>
2023-02-23 01:16:11 -0500

    Falta resolver el envio del excel

--- server/src/user/user.controller.ts
+++ server/src/user/user.controller.ts
@@ -3,11 +3,16 @@ import { CreateUserDTO } from './dto/create-user';
 import { UpdateUserDTO } from './dto/update-user';
 import { UserService } from './user.service';
 import { hash } from 'src/common/helpers/bcrypt.helpers';
+import { Response } from 'express';
 import { PeriodsCoursesService } from 'src/courses-periods/courses-periods.service';
 import { ProgressService } from 'src/progress/progress.service';
 import { Progress } from 'src/progress/entities/progress.entity';
 import { CreateProgressDTO } from './dto/create-progress';
 import { UpdateProgressDTO } from 'src/progress/dto/update-progress';
+import { Workbook } from 'excel4node';
+import { Grades } from './entities/userGrades';
+import { exists, existsSync } from 'fs';
+import { join, resolve } from 'path';
 
 @Controller('user')
 export class UserController {
@@ -33,20 +38,44 @@ export class UserController {
     return await this.userService.getProgressByUserId(id);
   }
 
-  async getGradesByAsignature(asignatureId: string, periodCourseId: number) {
+  async getGradesByAsignature(
+    asignatureId: string,
+    periodCourseId: number,
+    res: Response,
+  ) {
     const grades = await this.userService.getGradesByAsignature(
       asignatureId,
       periodCourseId,
     );
 
+    const ugrades = grades.filter(
+      (grade) => grade.periodCourseAsignatureUnitId,
+    );
+
     const agrades = grades
       .map((grade) => {
         if (grade.periodCourseAsignatureId) {
+          const nu = ugrades
+            .map((unit) => {
+              if (unit.userId === grade.userId) {
+                return unit;
+              }
+            })
+            .filter((unit) => unit);
+          const nota =
+            nu.reduce((acc, item) => {
+              if (grade.userId === item.userId) {
+                acc += item.nota ?? 0;
+              } else {
+                acc += 0;
+              }
+              return acc;
+            }, 0) / nu.length;
           return {
             id: grade.id,
             asignature: grade.periodCourseAsignature.asignature.name,
             course: grade.periodCourseAsignature.periodCourse.course.name,
-            nota: grade.nota,
+            nota: grade.nota ? (grade.nota + nota) / 2 : nota,
             userId: grade.user.id,
             username: grade.user.name,
             units: [],
@@ -54,9 +83,6 @@ export class UserController {
         }
       })
       .filter((grade) => grade);
-    const ugrades = grades.filter(
-      (grade) => grade.periodCourseAsignatureUnitId,
-    );
 
     const data = agrades.map((agrade) => {
       const units = ugrades
@@ -65,17 +91,65 @@ export class UserController {
             return {
               id: unit.id,
               name: unit.periodCourseAsignatureUnit.unit.name,
-              nota: unit.nota ?? 0,
+              nota: unit.nota ?? 'N/A',
               userId: unit.user.id,
             };
           }
         })
-        .filter((unit) => unit);
+        .filter((unit) => unit)
+        .sort((a, b) => a.name.localeCompare(b.name));
       agrade.units = units;
       return agrade;
     });
-    console.log(data);
-    return data;
+    const filename = await this.generateExcelAsignature(data, res);
+    console.log(resolve('./', filename + '.xlsx'));
+    /* if (existsSync(resolve('./', filename + '.xlsx'))) {
+      console.log('e');
+      res.setHeader(
+        'Content-Type',
+        'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+      );
+      res.setHeader('Content-Disposition', 'attachment; filename=' + filename);
+      res.sendFile(resolve('./', filename + '.xlsx'));
+      // res.end();
+    } */
+
+    return filename;
+  }
+
+  async generateExcelAsignature(data: Grades[], res: Response) {
+    try {
+      const wb = new Workbook();
+      const style = wb.createStyle({
+        font: {
+          color: '#000000',
+          size: 12,
+        },
+        numberFormat: '$#,##0.00; ($#,##0.00); -',
+      });
+      data.forEach((d) => {
+        const ws = wb.addWorksheet(d.username);
+        ws.cell(1, 1).string('Asignature').style(style);
+        ws.cell(1, 2).string(d.asignature);
+        ws.cell(2, 1).string('Estudiante').style(style);
+        ws.cell(2, 2).string(d.username);
+        ws.cell(3, 1).string('Nota').style(style);
+        ws.cell(3, 2).string(d.nota.toString());
+        d.units.forEach((grade, index) => {
+          ws.cell(index + 5, 1)
+            .string(grade.name)
+            .style(style);
+          ws.cell(index + 5, 2)
+            .string(grade.nota.toString())
+            .style(style);
+        });
+      });
+      const filename = `${data[0].course}_${data[0].asignature}`;
+      wb.write(filename + '.xlsx', res);
+      return filename;
+    } catch (e) {
+      console.log('lala', e);
+    }
   }
 
   async create(data: CreateUserDTO) {
